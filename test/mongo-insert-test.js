25a186b04fd3a8567b32de714059aea3d4f7a6d3
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-10-25 13:15:09 -0400

    Add: pushResults - improve tests

--- test/mongo-insert-test.js
+++ test/mongo-insert-test.js
@@ -2,43 +2,31 @@ var etl = require('../index'),
     inspect = require('./lib/inspect'),
     assert = require('assert'),
     data = require('./data'),
-    Promise = require('bluebird'),
-    mongo = require('mongodb');
+    mongo = require('./lib/mongo');
 
 
-Promise.promisifyAll(mongo);
-Promise.promisifyAll(mongo.MongoClient);
-
-var collection;
-
-before(function() {
-  return mongo.connectAsync('mongodb://localhost:27017')
-    .then(function(db)  {
-      collection = db.collection('etl-test');
-      return collection.removeAsync({});
-    },function(e) {
-      throw 'Unable to connect to local mongodb: '+e.message;
-    });
-});
-
-describe('mongo insert',function() {
-  it('pipes data into mongo',function() {
-    var mongo = etl.mongo.insert(collection);
-    data.stream().pipe(mongo);
-    return inspect(mongo);
+describe('mongo',function() {
+  it('insert piped data into mongo',function() {
+    return mongo.getCollection('insert')
+      .then(function(collection) {
+        var insert = etl.mongo.insert(collection,{pushResult:true});
+        data.stream().pipe(insert);
+        return inspect(insert);
+      })
+      .then(function(d) {
+        d.forEach(function(d) {
+          assert.deepEqual(d,{ok:1,n:1});
+        });
+      });
   });
 
-  it('reading data back',function() {
-    return collection.find({})
-      .toArrayAsync()
+  it('results are saved',function() {
+    return mongo.getCollection('insert',true)
+      .then(function(collection) {
+        return collection.find({},{_id:false}).toArrayAsync();
+      })
       .then(function(d) {
-        
-        d.forEach(function(d) {
-          d.dt = new Date(d.dt);
-          delete d._id;
-        });
-
         assert.deepEqual(d,data.data);
-    });
+      });
   });
 });

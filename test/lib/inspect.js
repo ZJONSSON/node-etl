2bc13e2d78db3fd00f2b92e96d36eed76c8c0494
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2015-10-25 17:33:41 -0400

    Throw error in inspect

--- test/lib/inspect.js
+++ test/lib/inspect.js
@@ -4,10 +4,13 @@ var streamz = require('streamz'),
 // Inspects the cumulative results of a stream
 module.exports = function(stream) {
   var res = [];
-  return new Promise(function(resolve) {
+  return new Promise(function(resolve,reject) {
     stream.pipe(streamz(function(d) {
       res.push(d);
     }))
+    .on('error',function(e) {
+      reject(e);
+    })
     .on('finish',function() {
       resolve(res);
     });

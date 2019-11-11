27f0704404fde6252dce52066207dda6329dda05
Nicholas <nickmisner@comcast.net>
2019-11-11 10:20:44 -0500

    Small testing changes for succesful upsert

---
+++ test/data-changed.js
@@ -0,0 +1,64 @@
+const PassThrough = require('stream').PassThrough;
+
+const data = {
+  data : [
+    { text: 'Nathaniel Olson 51    1/10/2025',
+      name: 'Nathaniel Olson',
+      age: 51,
+      dt: new Date('1/10/2025'),
+      __line: 1,
+      __filename: 'test.txt'
+    },
+    { text: 'Ann Ellis       35    10/2/2035',
+      name: 'Ann Ellis',
+      age: 35,
+      dt: new Date('10/2/2035'),
+      __line: 2,
+      __filename: 'test.txt'
+    },
+    { text: 'Willie Freeman  38     4/1/2016',
+      name: 'Willie Freeman',
+      age: 38,
+      dt: new Date('4/1/2016'),
+      __line: 3,
+      __filename: 'test.txt'
+    }
+  ],
+
+  fixed: ['Nathaniel O','lson 52    1/10/202','5Ann Ellis       36  ','  10/2/203','5Willie F','reeman  38     4/1/20','16'],
+  layout : { 
+    name : 16,
+    age : { length : 3,  transform: Number },
+    dt : { length : 12, transform: function(d) { return new Date(d); } }
+  }
+};
+
+data.copy = function() {
+  return this.data.map(function(d) {
+    return Object.keys(d).reduce(function(p,key) {
+      p[key] = d[key];
+      return p;
+    },{});
+  });
+};
+
+data.stream = function(options) {
+  const s = PassThrough({objectMode:true});
+  const data = this.copy();
+
+  data.forEach(function(d,i) {
+    setTimeout(function() {
+      s.write(options && options.clone ? Object.create(d) : d);
+      if (i == data.length-1)
+        s.end();
+    },i*10);
+  });
+  return s;
+};
+
+
+for (let key in data.data) 
+  Object.freeze(data.data[key]);
+
+
+module.exports = data;

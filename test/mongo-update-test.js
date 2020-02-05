d1a81dca1d934b58905abf8d419fc665a5b5a020
Ashish Modi <ashish.modi@rbi.co.uk>
2020-02-05 14:23:06 +0000

    fixed merge conflicts

--- test/mongo-update-test.js
+++ test/mongo-update-test.js
@@ -6,7 +6,7 @@ const t = require('tap');
 
 t.test('mongo update', {autoend: true}, t => {
 
-  t.teardown(() => clear());
+  t.teardown(() => t.end());
 
   t.test('single record', {autoend: true}, async t => {
     const collection = await getCollection('update-empty');
@@ -104,7 +104,6 @@ t.test('mongo update', {autoend: true}, t => {
           if (i) d.newfield = 'newfield';
           return d;
         });
-        console.log(d);
         t.same(d,expected,'results were saved');
       });
     });

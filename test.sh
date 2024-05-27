5c54e479c3ef12480bb444a38b8f35f327e7c3ba
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-05-27 12:30:37 -0400

    Github actions for testing and coverage

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ wait-for-it mongodb:27017
 wait-for-it mysql:3306
 wait-for-it postgres:5432
 # cluster test needs to be run from master thread
-node test/cluster-test.js | tap - test --exclude=*cluster* jobs=5 --passes
+node test/cluster-test.js | tap test --exclude=*cluster* jobs=5  --coverage-report=lcov --allow-incomplete-coverage

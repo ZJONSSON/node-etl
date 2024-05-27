6a0a7db71ae525ca9f0992e527500cb1dde923d5
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-05-27 20:07:32 +0000

    Github actions for testing and coverage (#124)

--- test/lib/dataStream.js
+++ test/lib/dataStream.js
@@ -2,10 +2,13 @@ const PassThrough = require('stream').PassThrough;
 
 module.exports = function dataStream(data) {
   const s = PassThrough({objectMode:true});
-  data.forEach((d,i) => setTimeout( () => {
-    s.write(d);
-    if (i == data.length-1)
-      s.end();
-  },i));
+  // Simulate async data stream
+  Promise.resolve().then(async() => {
+    for (let i = 0; i < data.length; i++) {
+      s.write(data[i]);
+      await new Promise(resolve => setTimeout(resolve,10))
+    }
+    s.end();
+  });
   return s;
 };
